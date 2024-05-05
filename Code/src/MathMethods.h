7f8b5f91cf9041ae4ffd88e74bbff4f9b5c2ecd6
PedroJoaoPereira <PedroJoaoPereira@users.noreply.github.com>
2024-05-05 10:13:03 +0100

    Removed deprecated methods

--- Code/src/MathMethods.h
+++ Code/src/MathMethods.h
@@ -1,10 +1,6 @@
 #ifndef MATH_METHODS_H
 #define MATH_METHODS_H
 
-// int num1 - integer value
-// int num2 - integer value
-// Return least common multiple of two integers
-int lcm(int num1, int num2);
 
 // int num1 - first value
 // int num2 - second value
@@ -18,11 +14,5 @@ int greatestDivisor(int num);
 // double value - value to be rounded
 // Fast round a value
 int roundFast(double value);
-
-// double* val  - value to be clamped
-// double min   - minimum limit of clamping
-// double max   - maximum limit of clamping
-// Clamp a value to a defined interval
-void clamp(double &val, double min, double max);
 
 #endif
