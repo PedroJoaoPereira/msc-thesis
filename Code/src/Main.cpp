e5115e798d931735c4224b68fe4417625e2177b2
PedroJoaoPereira <PedroJoaoPereira@users.noreply.github.com>
2024-05-05 10:12:56 +0100

    Finished bilinear, bicubic has some error in calculating original pixels

--- Code/src/Main.cpp
+++ Code/src/Main.cpp
@@ -21,6 +21,8 @@ int main(){
     // HD720  - 1280 x 720
     // -----------------------------------------------
     // INFO OF IMAGES USED IN TESTS ------------------
+    ImageInfo imgDebug("imgs/lin-yuv422p-6x6.yuv", 6, 6, AV_PIX_FMT_YUV422P);
+
     ImageInfo img01("imgs/uyvy422-7680x4320.yuv", 7680, 4320, AV_PIX_FMT_UYVY422);
     ImageInfo img02("imgs/yuv420p-7680x4320.yuv", 7680, 4320, AV_PIX_FMT_YUV420P);
     ImageInfo img03("imgs/yuv422p-7680x4320.yuv", 7680, 4320, AV_PIX_FMT_YUV422P);
@@ -28,8 +30,9 @@ int main(){
     // -----------------------------------------------
     // DEBUG VARIABLES -------------------------------
     int nTimes = 1;
-    int operation = SWS_BILINEAR;
-    ImageInfo outImg("imgs/output.yuv", 1280, 720, AV_PIX_FMT_YUV422P);
+    int operation = SWS_BICUBIC;
+    ImageInfo outImg("imgs/output.yuv", 12, 12, AV_PIX_FMT_YUV422P);
+    //ImageInfo outImg("imgs/output.yuv", 3840, 2160, AV_PIX_FMT_YUV422P);
     // -----------------------------------------------
     // SCALING OPERATIONS ----------------------------
     // Initialize ffmpeg
@@ -37,7 +40,8 @@ int main(){
 
     // Apply the operations
     while(nTimes > 0){
-        int executionTime = ffmpeg_scale(img01, outImg, operation);
+        //int executionTime = ffmpeg_scale(imgDebug, outImg, operation);
+        int executionTime = sequential_scale(imgDebug, outImg, operation);
         //int executionTime = sequential_scale(img01, outImg, operation);
         if(executionTime < 0){
             cerr << "Could not execute the scaling method!" << endl;
